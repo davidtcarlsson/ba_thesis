3baba83a3491eb012d8bbc81879112607f16fd41
David Carlsson <57452866+davidtcarlsson@users.noreply.github.com>
2022-04-26 09:14:38 +0200

    Added a function that creates a figure with  a given number of models for a given alpha

---
+++ R/create_plots.R
@@ -0,0 +1,10 @@
+plot_models <- function(data, cost_funs, alpha) {
+  # Plots the models for a given alpha
+  preds <- model_preds(data = data, cost_funs = cost_funs, alpha = alpha)
+  preds_m <- melt(preds, id.vars = "x", measure.vars = -c(1, 2))
+  
+  ggplot() +
+    geom_line(data = preds, aes(x, y)) + 
+    geom_line(data = preds_m, aes(x = x, y = value, col = variable)) +
+    labs(title = paste("alpha =", alpha), col = "Method")
+}
