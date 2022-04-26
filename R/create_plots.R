3b020e1d36a66b310a16ad9d0922fd814b2eee61
David Carlsson <57452866+davidtcarlsson@users.noreply.github.com>
2022-04-26 10:19:02 +0200

    Added a function to plot the cost functions

--- R/create_plots.R
+++ R/create_plots.R
@@ -8,3 +8,20 @@ plot_models <- function(data, cost_funs, alpha) {
     geom_line(data = preds_m, aes(x = x, y = value, col = variable)) +
     labs(title = paste("Dataset ", comment(data),", alpha = ", alpha, sep = ""), col = "Method")
 }
+
+plot_cost_funs <- function(xlim, alpha) {
+  Error <- seq(xlim[1], xlim[2], by = 0.01)
+  df <- data.frame(
+    Error,
+    OLS = Error^2,
+    LAD = abs(Error),
+    LinLin = ifelse(Error > 0, alpha * Error, abs(Error)),
+    QuadQuad = ifelse(Error > 0, alpha * Error^2, Error^2)
+  )
+  
+  df_m <- melt(df, id.vars = "Error", measure.vars = c(-1))
+  
+  ggplot() +
+    geom_line(data = df_m, aes(x = Error, y = value, col = variable)) +
+    labs(title = paste("Shape of the different loss functions,", "alpha =", alpha), y = "Loss", col = "Methods")
+}
