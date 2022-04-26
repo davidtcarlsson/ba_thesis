b56e0693c1d824022c552cb90d85c96a5d384b45
David Carlsson <57452866+davidtcarlsson@users.noreply.github.com>
2022-04-26 09:15:44 +0200

    Added a function to create a linear dataset with normal errors

--- R/generate_data.R
+++ R/generate_data.R
@@ -1,10 +1,21 @@
-random_walk <- function(N, x0, mu, variance) {
+generate_rw <- function(N, x0, mu, variance) {
   z <- cumsum(rnorm(n = N, mean = 0, sd = sqrt(variance)))
   t <- 1:N
   y <- x0 + t * mu + z
   
   data.frame(
     x = t,
-    y = y
+    y
   )
 }
+
+generate_lin <- function(N, x0, mu, variance) {
+  t <- 1:N
+  y <- x0 + t + rnorm(N, mean = mu, sd = sqrt(variance))
+  
+  data.frame(
+    x = t,
+    y
+  )
+}
+
