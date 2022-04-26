c8c9ca37cbca62c65583be255db2ed0748a2a439
David Carlsson <57452866+davidtcarlsson@users.noreply.github.com>
2022-04-26 09:16:09 +0200

    Added the second dataset to the analysis

--- analysis.R
+++ analysis.R
@@ -7,18 +7,56 @@ source("R/create_plots.R")
 
 set.seed(2022)
 
-# Lets create a dataset
+# Lets create a two datasets
+# - df1: Random walk 
+# - df2: Linear with normal error
 sample_size <- 50
-df1 <- random_walk(N = sample_size, x0 = 0, mu = 1, variance = 5)
+df1 <- generate_rw(N = sample_size, x0 = 0, mu = 1, variance = 5)
+df2 <- generate_lin(N = sample_size, x0 = 0, mu = 0, variance = 5)
 
-preds_1 <- model_preds(data = df1, list(ols, lad, linlin), alpha = 2)
-preds_2 <- model_preds(data = df1, list(ols, lad, linlin), alpha = 5)
-preds_3 <- model_preds(data = df1, list(ols, lad, linlin), alpha = 10)
+ggplot() +
+  geom_line(data = df1, aes(x, y, col = "Dataset 1")) +
+  geom_line(data = df2, aes(x, y, col = "Dataset 2")) +
+  labs(title = "Simulated datasets", col = "")
 
-plot_models(preds_1)
-plot_models(preds_2)
-plot_models(preds_3)
+# Parameters used for the first dataset
+data <- df1
+cost_funs <- c(ols, lad, linlin, quadquad)
 
-model_eval(preds_1, list(linlin))
-model_eval(preds_2, list(linlin))
-model_eval(preds_3, list(linlin))
+# Table where we use the linlin as the evaluation metric
+create_table(data = data, 
+             cost_funs = cost_funs, 
+             alphas = c(1, 2, 5, 10, 50), 
+             metric = linlin)
+
+# Table where we use the quadquad as the evaluation metric
+create_table(data = data, 
+             cost_funs = cost_funs, 
+             alphas = c(1, 2, 5, 10, 50), 
+             metric = quadquad)
+
+# We can also plot the models for a given alpha
+plot_models(data = data,
+            cost_funs = cost_funs,
+            alpha = 50)
+
+# Parameters used for the second dataset
+data <- df2
+cost_funs <- c(ols, lad, linlin, quadquad)
+
+# Table where we use the linlin as the evaluation metric
+create_table(data = data, 
+             cost_funs = cost_funs, 
+             alphas = c(1, 2, 5, 10, 50), 
+             metric = linlin)
+
+# Table where we use the quadquad as the evaluation metric
+create_table(data = data, 
+             cost_funs = cost_funs, 
+             alphas = c(1, 2, 5, 10, 50), 
+             metric = quadquad)
+
+# We can also plot the models for a given alpha
+plot_models(data = data,
+            cost_funs = cost_funs,
+            alpha = 5)
