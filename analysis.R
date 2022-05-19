9c4274693e334d4fe6647c4a710b85b847807d8a
David Carlsson <57452866+davidtcarlsson@users.noreply.github.com>
2022-05-19 14:01:39 +0200

    Starting to move the analysis to the thesis rmd file instead

--- analysis.R
+++ analysis.R
@@ -21,118 +21,118 @@ comment(df1) <- "1"
 df2 <- generate_rw(N = sample_size, step_size = step_size)
 comment(df2) <- "2"
 
-ggplot() +
-  geom_line(data = df1, aes(x, y, col = "Dataset 1")) +
-  geom_line(data = df2, aes(x, y, col = "Dataset 2")) +
-  labs(title = "Simulated datasets", col = "", x = "t")
-plot_save("simulated_datasets.png")
+# ggplot() +
+#   geom_line(data = df1, aes(x, y, col = "Dataset 1")) +
+#   geom_line(data = df2, aes(x, y, col = "Dataset 2")) +
+#   labs(title = "Simulated datasets", col = "", x = "t")
+#plot_save("simulated_datasets.png")
 
 # Since we use the same cost functions throughout the analysis i will define them
 # here
-cost_funs <- c(ols, lad, quadquad, linlin)
-alphas <- c(1, 2, 5, 10)
-
-plot_loss_alpha(df1, cost_funs = cost_funs, c(1, tail(alphas, 1)), linlin) 
-plot_save("df1_loss_diff_alphas_linlin.png")
-
-plot_loss_alpha(df1, cost_funs = cost_funs, c(1, tail(alphas, 1)), quadquad) 
-plot_save("df1_loss_diff_alphas_quadquad.png")
-
-plot_loss_alpha(df2, cost_funs = cost_funs, c(1, tail(alphas, 1)), linlin) 
-plot_save("df2_loss_diff_alphas_linlin.png")
-
-plot_loss_alpha(df2, cost_funs = cost_funs, c(1, tail(alphas, 1)), quadquad) 
-plot_save("df2_loss_diff_alphas_quadquad.png")
-
-# Table where we use the linlin as the evaluation metric
-df1_eval_table_linlin <- create_table(data = df1, 
-                                  cost_funs = cost_funs, 
-                                  alphas = alphas, 
-                                  metric = linlin)
-write.xlsx(df1_eval_table_linlin, "output/df1_eval_table_linlin.xlsx", rowNames = TRUE)
-
-# Table where we use the quadquad as the evaluation metric
-df1_eval_table_quadquad <- create_table(data = df1, 
-                                  cost_funs = cost_funs, 
-                                  alphas = alphas, 
-                                  metric = quadquad)
-write.xlsx(df1_eval_table_quadquad, "output/df1_eval_table_quadquad.xlsx", rowNames = TRUE)
-
-# Table where we use the linlin as the evaluation metric
-df2_eval_table_linlin <- create_table(data = df2, 
-                                      cost_funs = cost_funs, 
-                                      alphas = alphas, 
-                                      metric = linlin)
-write.xlsx(df2_eval_table_linlin, "output/df2_eval_table_linlin.xlsx", rowNames = TRUE)
-
-# Table where we use the quadquad as the evaluation metric
-df2_eval_table_quadquad <- create_table(data = df2, 
-                                        cost_funs = cost_funs, 
-                                        alphas = alphas, 
-                                        metric = quadquad)
-write.xlsx(df2_eval_table_quadquad, "output/df2_eval_table_quadquad.xlsx", rowNames = TRUE)
-
-# We can also plot the models for a given alpha
-plot_models(data = df1,
-            cost_funs = cost_funs,
-            alpha = 1)
-plot_save("df1_alpha_1.png")
-
-plot_models(data = df1,
-            cost_funs = cost_funs,
-            alpha = 2)
-plot_save("df1_alpha_2.png")
-
-plot_models(data = df1,
-            cost_funs = cost_funs,
-            alpha = 5)
-plot_save("df1_alpha_5.png")
-
-plot_models(data = df1,
-            cost_funs = cost_funs,
-            alpha = 10)
-plot_save("df1_alpha_10.png")
-
-# Now lets do the same but for dataset 2
-plot_models(data = df2,
-            cost_funs = cost_funs,
-            alpha = 1)
-plot_save("df2_alpha_1.png")
-
-plot_models(data = df2,
-            cost_funs = cost_funs,
-            alpha = 2)
-plot_save("df2_alpha_2.png")
-
-plot_models(data = df2,
-            cost_funs = cost_funs,
-            alpha = 5)
-plot_save("df2_alpha_5.png")
-
-plot_models(data = df2,
-            cost_funs = cost_funs,
-            alpha = 10)
-plot_save("df2_alpha_10.png")
-
-# I also want to plot the different cost functions to get an idea of their shape
-plot_loss_funs(xlim = c(-5, 5), alpha = 2)
-plot_save("cost_funs_alpha_2.png")
-
-plot_loss_funs(xlim = c(-5, 5), alpha = 5)
-plot_save("cost_funs_alpha_5.png")
-
-plot_loss_funs(xlim = c(-5, 5), alpha = 10)
-plot_save("cost_funs_alpha_10.png")
-
-plot_a_loss(c(-5, 5), ols, c(2, 5, 10))
-plot_save("ols_loss.png")
-
-plot_a_loss(c(-5, 5), lad, c(2, 5, 10))
-plot_save("lad_loss.png")
-
-plot_a_loss(c(-5, 5), linlin, c(2, 5, 10))
-plot_save("linlin_loss.png")
-
-plot_a_loss(c(-5, 5), quadquad, c(2, 5, 10))
-plot_save("quadquad_loss.png")
-
+# cost_funs <- c(ols, lad, quadquad, linlin)
+# alphas <- c(1, 2, 5, 10)
+# 
+# plot_loss_alpha(df1, cost_funs = cost_funs, c(1, tail(alphas, 1)), linlin) 
+# plot_save("df1_loss_diff_alphas_linlin.png")
+# 
+# plot_loss_alpha(df1, cost_funs = cost_funs, c(1, tail(alphas, 1)), quadquad) 
+# plot_save("df1_loss_diff_alphas_quadquad.png")
+# 
+# plot_loss_alpha(df2, cost_funs = cost_funs, c(1, tail(alphas, 1)), linlin) 
+# plot_save("df2_loss_diff_alphas_linlin.png")
+# 
+# plot_loss_alpha(df2, cost_funs = cost_funs, c(1, tail(alphas, 1)), quadquad) 
+# plot_save("df2_loss_diff_alphas_quadquad.png")
+# 
+# # Table where we use the linlin as the evaluation metric
+# df1_eval_table_linlin <- create_table(data = df1, 
+#                                   cost_funs = cost_funs, 
+#                                   alphas = alphas, 
+#                                   metric = linlin)
+# write.xlsx(df1_eval_table_linlin, "output/df1_eval_table_linlin.xlsx", rowNames = TRUE)
+# 
+# # Table where we use the quadquad as the evaluation metric
+# df1_eval_table_quadquad <- create_table(data = df1, 
+#                                   cost_funs = cost_funs, 
+#                                   alphas = alphas, 
+#                                   metric = quadquad)
+# write.xlsx(df1_eval_table_quadquad, "output/df1_eval_table_quadquad.xlsx", rowNames = TRUE)
+# 
+# # Table where we use the linlin as the evaluation metric
+# df2_eval_table_linlin <- create_table(data = df2, 
+#                                       cost_funs = cost_funs, 
+#                                       alphas = alphas, 
+#                                       metric = linlin)
+# write.xlsx(df2_eval_table_linlin, "output/df2_eval_table_linlin.xlsx", rowNames = TRUE)
+# 
+# # Table where we use the quadquad as the evaluation metric
+# df2_eval_table_quadquad <- create_table(data = df2, 
+#                                         cost_funs = cost_funs, 
+#                                         alphas = alphas, 
+#                                         metric = quadquad)
+# write.xlsx(df2_eval_table_quadquad, "output/df2_eval_table_quadquad.xlsx", rowNames = TRUE)
+# 
+# # We can also plot the models for a given alpha
+# plot_models(data = df1,
+#             cost_funs = cost_funs,
+#             alpha = 1)
+# plot_save("df1_alpha_1.png")
+# 
+# plot_models(data = df1,
+#             cost_funs = cost_funs,
+#             alpha = 2)
+# plot_save("df1_alpha_2.png")
+# 
+# plot_models(data = df1,
+#             cost_funs = cost_funs,
+#             alpha = 5)
+# plot_save("df1_alpha_5.png")
+# 
+# plot_models(data = df1,
+#             cost_funs = cost_funs,
+#             alpha = 10)
+# plot_save("df1_alpha_10.png")
+# 
+# # Now lets do the same but for dataset 2
+# plot_models(data = df2,
+#             cost_funs = cost_funs,
+#             alpha = 1)
+# plot_save("df2_alpha_1.png")
+# 
+# plot_models(data = df2,
+#             cost_funs = cost_funs,
+#             alpha = 2)
+# plot_save("df2_alpha_2.png")
+# 
+# plot_models(data = df2,
+#             cost_funs = cost_funs,
+#             alpha = 5)
+# plot_save("df2_alpha_5.png")
+# 
+# plot_models(data = df2,
+#             cost_funs = cost_funs,
+#             alpha = 10)
+# plot_save("df2_alpha_10.png")
+# 
+# # I also want to plot the different cost functions to get an idea of their shape
+# plot_loss_funs(xlim = c(-5, 5), alpha = 2)
+# plot_save("cost_funs_alpha_2.png")
+# 
+# plot_loss_funs(xlim = c(-5, 5), alpha = 5)
+# plot_save("cost_funs_alpha_5.png")
+# 
+# plot_loss_funs(xlim = c(-5, 5), alpha = 10)
+# plot_save("cost_funs_alpha_10.png")
+# 
+# plot_a_loss(c(-5, 5), ols, c(2, 5, 10))
+# plot_save("ols_loss.png")
+# 
+# plot_a_loss(c(-5, 5), lad, c(2, 5, 10))
+# plot_save("lad_loss.png")
+# 
+# plot_a_loss(c(-5, 5), linlin, c(2, 5, 10))
+# plot_save("linlin_loss.png")
+# 
+# plot_a_loss(c(-5, 5), quadquad, c(2, 5, 10))
+# plot_save("quadquad_loss.png")
+# 
